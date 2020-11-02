81c9500745b6722d81d857dcd2656b79986dc73b
Billy Downing <wldowning@gmail.com>
2020-11-02 12:18:02 -0700

    completed fetch posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,4 +3,6 @@ import postsReducer from './postsReducer';
 
 export default combineReducers({
   posts: postsReducer
+  // State will have a property called posts in PostList component
+  // This property will hold all the data that this reducer has returned
 });
